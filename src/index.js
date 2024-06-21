602e639a39f013829b8d5fe17b64aedee82d8efe
KuriyamaMirai <lovef@outlook.com>
2024-06-21 14:08:27 +0800

    update

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.devppm.com": dockerHub,
+  "quay.devppm.com": "https://quay.io",
+  "gcr.devppm.com": "https://gcr.io",
+  "k8s-gcr.devppm.com": "https://k8s.gcr.io",
+  "k8s.devppm.com": "https://registry.k8s.io",
+  "ghcr.devppm.com": "https://ghcr.io",
+  "cloudsmith.devppm.com": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.devppm.com": dockerHub,
 };
 
 function routeByHosts(host) {
